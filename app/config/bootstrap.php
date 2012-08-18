e130458b653403939ff21f7fdaa79e5afa9a0377
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-18 11:46:47 +0900

    Cleanup code

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,18 +1,16 @@
 <?php
-// アプリケーション
+// application
 require_once APP_DIR.'app_controller.php';
 require_once APP_DIR.'app_model.php';
 require_once APP_DIR.'app_exception.php';
 
+// helpers
 require_once HELPERS_DIR.'html_helper.php';
 
-// autoload
 spl_autoload_register(function($name) {
     $filename = Inflector::underscore($name) . '.php';
     if (strpos($name, 'Controller') !== false) {
         require CONTROLLERS_DIR . $filename;
-    } elseif (strpos($name, 'Master') !== false) {
-        require MASTER_DIR . $filename;
     } else {
         if (file_exists(MODELS_DIR . $filename)) {
             require MODELS_DIR . $filename;
--- app/config/core_production.php
+++ app/config/core_production.php
@@ -1,9 +1,8 @@
 <?php
-// 実行環境
 define('ENV_PRODUCTION', true);
-define('APP_HOST', 'hello.example.net');
+define('APP_HOST', 'hello.example.com');
 define('APP_BASE_PATH', '/');
-define('APP_URL', 'http://hello.example.net/');
+define('APP_URL', 'http://hello.example.com/');
 
 ini_set('error_log', LOGS_DIR.'php.log');
 ini_set('session.auto_start', 0);
