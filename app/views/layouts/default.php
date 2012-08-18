19eb7788bf67102d450477acc3c45c8a80c46f01
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-18 18:27:16 +0900

    Fix raw output

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>DietCake <?php echo $title ?></title>
+    <title>DietCake <?php eh($title) ?></title>
 
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <style>
