1bdce7583159fcdf80efd393bcc0ffe9fd5ee04a
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-19 18:11:01 +0900

    Add core.php setting

--- app/config/core_development.php
+++ app/config/core_development.php
@@ -1,10 +1,10 @@
 <?php
-define('ENV_PRODUCTION', true);
+define('ENV_PRODUCTION', false);
 define('APP_HOST', 'hello.example.com');
 define('APP_BASE_PATH', '/');
 define('APP_URL', 'http://hello.example.com/');
 
 error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 'Off');
+ini_set('display_errors', 'On');
 ini_set('error_log', LOGS_DIR.'php.log');
 ini_set('session.auto_start', 0);
